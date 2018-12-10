773621c6d8a8265d64e546bd92c5da12c40b8156
cmor2207 <celine.reisser@gmail.com>
2018-12-09 22:19:41 -1000

    Create 07b_vcftools_missing_Ind.sh

---
+++ 00_scripts/02_snps/07b_vcftools_missing_Ind.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#PBS -q
+#PBS -l walltime=24:00:00
+#PBS -l mem=115g
+#PBS -N vcftools
+
+WORKDIR=gamma
+INDIR=07_snps/02_freebayes
+VCFTOOLSENV=
+
+
+cd $WORKDIR/$INDIR
+
+#Remove individuals taht had more than 15% missing data.
+$VCFTOOLSENV/vcftools --remove-indv HI.4880.007.NEBNext_dual_i7_C1---NEBNext_dual_i5_C1.61 --remove-indv HI.4880.007.NEBNext_dual_i7_G1---NEBNext_dual_i5_G1.9 --remove-indv HI.4880.007.NEBNext_dual_i7_H12---NEBNext_dual_i5_H12.25 --remove-indv HI.4880.007.NEBNext_dual_i7_D12---NEBNext_dual_i5_D12.44 --vcf $INDIR/DP10_SNP_MAF0.1_miss0.1_GAMMA_genome_biallelic.vcf --recode --out $INDIR/DP10_SNP_MAF0.1_miss0.1_GAMMA_genome_biallelic_IndFiltered
+
